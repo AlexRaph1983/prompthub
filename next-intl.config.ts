c1afbe61ba8dd60f7f36c833ddc0491d955b3a3c
Alex <alex@example.com>
2025-09-18 00:01:23 +0700

    i18n: defaultLocale=ru; feat: add /[locale]/auth/signin with Google

--- next-intl.config.ts
+++ next-intl.config.ts
@@ -2,7 +2,7 @@ import {getRequestConfig} from 'next-intl/server';
 
 export default getRequestConfig(async ({locale}) => ({
   locales: ['en', 'ru'],
-  defaultLocale: 'en',
+  defaultLocale: 'ru',
   messages: (await import(`./messages/${locale}.json`)).default
 }));
 
