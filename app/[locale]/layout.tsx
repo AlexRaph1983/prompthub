58a49ab85df57cd709d392d6063134e7ab2962bb
Alex <alex@example.com>
2025-10-11 00:00:25 +0700

    Fix HTML structure conflicts - Remove duplicate HTML tags and Yandex.Metrika - Fix layout hierarchy for proper HTML validation - Add dynamic language setting

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -70,34 +70,35 @@ export default async function LocaleLayout({ children, params }: LayoutProps) {
   const messages = (await import(`@/messages/${locale}.json`)).default as any;
 
   return (
-    <html lang={locale} suppressHydrationWarning>
-      <body className={inter.className}>
-        {/* Yandex.Metrika */}
-        <Script id="yandex-metrika" strategy="afterInteractive">{`
-          (function(m,e,t,r,i,k,a){
-              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104142063', 'ym');
-          ym(104142063, 'init', { ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true });
-        `}</Script>
-        <noscript><div><img src="https://mc.yandex.ru/watch/104142063" style={{position:'absolute', left:'-9999px'}} alt="" /></div></noscript>
+    <>
+      <script dangerouslySetInnerHTML={{
+        __html: `document.documentElement.lang = '${locale}';`
+      }} />
+      {/* Yandex.Metrika */}
+      <Script id="yandex-metrika" strategy="afterInteractive">{`
+        (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104142063', 'ym');
+        ym(104142063, 'init', { ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true });
+      `}</Script>
+      <noscript><div><img src="https://mc.yandex.ru/watch/104142063" style={{position:'absolute', left:'-9999px'}} alt="" /></div></noscript>
 
-        <ClientProviders>
-          <NextIntlClientProvider locale={locale} messages={messages}>
-            <AuthProvider>
-              <PromptProvider>
-                <Navigation />
-                <div className="min-h-screen">
-                  {children}
-                </div>
-                <AddPromptModal />
-              </PromptProvider>
-            </AuthProvider>
-          </NextIntlClientProvider>
-        </ClientProviders>
-      </body>
-    </html>
+      <ClientProviders>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <AuthProvider>
+            <PromptProvider>
+              <Navigation />
+              <div className="min-h-screen">
+                {children}
+              </div>
+              <AddPromptModal />
+            </PromptProvider>
+          </AuthProvider>
+        </NextIntlClientProvider>
+      </ClientProviders>
+    </>
   );
 }
